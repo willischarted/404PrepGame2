17df842fb312a296b6c18be7dee2d078b2649fa4
William <willischarted@gmail.com>
2018-09-14 19:27:25 -0400

    Game Project Upload
    Actual Game Project upload

---
+++ PrepGame1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	
+	public float secondTextWaitMin;
+	public float secondTextWaitMax;
+
+	public float thirdTextWaitMin;
+	public float thirdTextWaitMax;
+
+	public GameObject textPrompt;
+	private TextController textController;
+
+	public GameObject player1;
+	private bool player1Hit;
+	public GameObject player2;
+	private bool player2Hit;
+
+
+	public GameObject bullet1;
+	public GameObject bullet2;
+
+	private bool gameOver = false;
+	private GameObject winner;
+
+
+
+	private int gameState;
+	/*
+	1 is "Ready" state
+	2 is "Set" state
+	3 is "Go!" state
+	*/
+
+	void Awake(){
+		textController = textPrompt.GetComponent<TextController>();
+		if (textController == null)
+			Debug.Log("Could not find textController");
+	}
+	// Use this for initialization
+	void Start () {
+		
+		gameState = 1;
+		player1Hit = false;
+		player2Hit = false;
+		//bool gameOver = false;
+		textController.updateText(gameState);
+		StartCoroutine("setGameState");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameOver){
+			if (Input.GetKeyDown(KeyCode.A)){
+				if (gameState != 3){
+					gameOver = true;
+					winner = player2;
+					FinishGame();
+
+				}
+				bullet1.GetComponent<BulletController>().setIsFire(true);
+				// the other player lost -> dont need colliders, its just who pressed first.
+				//winner = player1;
+				//gameOver = true;
+				//StopCoroutine("setGameState");
+				//textController.updateGameOver("Game Over! " + winner.name + "Won!");
+
+			}
+
+			if (Input.GetKeyDown(KeyCode.L)){
+				if (gameState != 3){
+					gameOver = true;
+					winner = player1;
+					FinishGame();
+
+				}
+				// the other player lost -> dont need colliders, its just who pressed first.
+				//winner = player1;
+				//gameOver = true;
+				//StopCoroutine("setGameState");
+				//textController.updateGameOver("Game Over! " + winner.name + "Won!");
+
+				bullet2.GetComponent<BulletController>().setIsFire(true);
+
+			}
+
+			
+
+		
+		}
+		
+		if (Input.GetKeyDown(KeyCode.R)){
+			SceneManager.LoadScene("Main", LoadSceneMode.Single);
+		}
+
+
+	}
+
+	IEnumerator setGameState() {
+		yield return new WaitForSeconds(Random.Range(secondTextWaitMin,secondTextWaitMax));
+		gameState = 2;
+		textController.updateText(gameState);
+		//Set text to --
+		yield return new WaitForSeconds(Random.Range(thirdTextWaitMin,thirdTextWaitMax));
+		gameState = 3;
+		textController.updateText(gameState);
+		//set text to --
+
+	}
+
+	public void setWinner(GameObject gameWinner) {
+		winner = gameWinner;
+	}
+
+	public GameObject getWinner(){
+		return winner;
+	}
+
+	public void setGameOver(bool over) {
+		gameOver = over;
+	}
+
+	public bool getGameOver() {
+		return gameOver;
+	}
+
+	public void FinishGame() {
+		StopCoroutine("setGameState");
+		textController.updateGameOver("Game Over! " + winner.name + "Won! Press R to restart.");
+	}
+
+}
---
+++ PrepGame1/Assets/Scripts/TextController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public string text1;
+	public string text2;
+
+	public string text3;
+
+
+	private Text gameText;
+
+
+
+	void Awake(){
+		gameText = GetComponent<Text>();
+		if (gameText == null)
+			Debug.Log("Could not find gameText!");
+		//Debug.Log("done textcontroller");
+	}
+
+	// Use this for initialization
+	void Start () { 
+		
+	}
+	
+	public void updateText(int gameState) {
+		switch(gameState){
+			case 1:
+				//Debug.Log("start");
+				gameText.text = text1;
+				break;
+			case 2:
+				gameText.text = text2;
+				break;
+			case 3:
+				gameText.text = text3;
+				break;
+			default:
+				break;
+
+
+		}
+	}
+
+	public void updateGameOver(string text){
+		gameText.text = text;
+	}
+
+	
+}
